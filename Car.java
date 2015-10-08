7f3f7af5accad7c086b4c52adefca4ace0c895bb
Teresa Lamb <flour1337@gmail.com>
2015-10-08 00:58:55 -0400

    car code is finally working

--- Car.java
+++ Car.java
@@ -7,26 +7,69 @@ Run 'Car'
 import java.util.Scanner;
 
 public class Car {
-	Scanner userInput = new Scanner(System.in);
-	boolean ignition = false;
-	char colorChar = colorAssign();
-	String colorString = "";
-	int carX = randomPosition();
-	int carY = randomPosition();
 
-	public static void main(String args[]){		
-		
-		Car car = new Car();
-		car.reportStates();
+	public static void main(String args[]){	
+		Scanner userInput = new Scanner(System.in);			
+		boolean ignition = false;
+		char colorChar = colorAssign();
+		String colorString = "";
+		String input = "";
+		int carX = randomPosition();
+		int carY = randomPosition();
+		int moveX = 0;
+		int moveY = 0;
+		boolean running = true;
+		boolean movingCar = false;
+
+		while (running) {
+			reportStates(colorChar, ignition, carX, carY);
+			System.out.println("What would you like to do?");
+			System.out.println("1: turn the ignition on/off");
+			System.out.println("2: change the position of car");
+			System.out.println("Q: quit this program");
+			input = userInput.next();
+
+			switch(input) {
+				case "1": ignition = ignitionRemix(ignition);
+						break;
+				case "2": movingCar = true;
+						break;
+				case "Q": running = false;
+						break;
+				default: System.out.println("Error: Invalid Direction");
+						break;
+			}
+
+			if (movingCar) {
+				System.out.println("In which direction would you like to move the car?");
+				System.out.println("H: horizontal");
+				System.out.println("V: vertical");
+				String inputMove = userInput.next();
+
+				if (inputMove.equals("H")) {
+					System.out.println("How far left (negative value) or right (positive value) would you like to move?");
+					moveX = userInput.nextInt();
+					carX = moveCarX(ignition, carX, moveX);
+
+				} else if (inputMove.equals("V")) {
+					System.out.println("How far down (negative value) or up (positive value) would you like to move?");
+					moveY  = userInput.nextInt();
+					carY = moveCarY(ignition, carY, moveY);
+				} else {
+					System.out.println("Error: Invalid Direction");
+				}
+				movingCar = false;
+			}
+		}
 
 	}
 
-	public static int randomPosition() {
+	static int randomPosition() {
 		int a = (int )(Math.random() * 20 + 1);
 		return a;
 	}
 
-	public static char colorAssign() {	
+	static char colorAssign() {	
 		int b = (int )(Math.random() * 5 + 1);
 		char c;
 
@@ -41,29 +84,50 @@ public class Car {
 					break;
 			case 5: c = 'S'; //silver
 					break;
-			default: c = 'Z'; //undefined
+			default: c = 'U'; //undefined
 					break;
 		}
 		return c;
 	}
 
-	public static int moveCarX() {
-		return 0;
+	static int moveCarX(boolean inIgnition, int inCarX, int inMoveX) {
+		if (inIgnition){
+			if ((inMoveX + inCarX) <= 20){
+				inCarX = inMoveX + inCarX;
+			} else {
+				System.out.println("You cannot move the car out of the grid.");
+			}
+		} else {
+			System.out.println("You must turn on the ignition.");
+		}
+		return inCarX;
 	}
 
-	public static int moveCarY() {
-		return 0;
+	static int moveCarY(boolean inIgnition, int inCarY, int inMoveY) {
+		if (inIgnition){
+			if ((inMoveY + inCarY) <= 20) {
+				inCarY = inMoveY + inCarY;
+			} else {
+				System.out.println("You cannot move the car out of the grid.");
+			}	
+		} else {
+			System.out.println("You must turn on the ignition.");
+		}
+		return inCarY;
 	}
 
-	public static void ignitionState() {
-
+	static boolean ignitionRemix(boolean inIgnition) {
+		//return the opposite ignition state
+		inIgnition = !inIgnition;
+		return inIgnition;
 	}
 
-	public void reportStates() {
+	static void reportStates(char inColorChar, boolean inIgnition, int inCarX, int inCarY) {
 		System.out.println("Car Information");
+		String colorString;
 
 		//Print colorString based on Char
-		switch(colorChar) {
+		switch(inColorChar) {
 			case'R': colorString = "red";
 					break;
 			case'G': colorString = "green";
@@ -80,14 +144,14 @@ public class Car {
 		System.out.println("Color: " + colorString);
 
 		//print ignition state
-		if (ignition) {
+		if (inIgnition) {
 			System.out.println("Ignition: on");
 		} else {
 			System.out.println("Ignition: off");
 		}
 
 		//print car location
-		System.out.println("Location: (" + carX + ", " + carY + ")");
+		System.out.println("Location: (" + inCarX + ", " + inCarY + ")");
 
 		//print grid
 		for (int i = 0; i < 20; i++) {
@@ -98,8 +162,8 @@ public class Car {
 			System.out.print("- ");
 
 			for (int j = 0; j < 20; j++){
-				if (i == (carX - 1) && j == (carY - 1)){
-					System.out.print("R ");
+				if (j == (inCarX - 1) && i == (inCarY - 1)){
+					System.out.print(inColorChar + " ");
 				} else {
 					System.out.print("- ");
 				}
